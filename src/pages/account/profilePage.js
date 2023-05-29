8f599ecf92ef34b7630b446921b917fe2e9b4b28
Alinochka07 <alina011988@icloud.com>
2023-05-29 21:34:45 +0600

    feat: profile page added

---
+++ src/pages/account/profilePage.js
@@ -0,0 +1,43 @@
+import { signOut } from "firebase/auth";
+import { auth } from "../../index";
+import MainHeader from "../../components/1_main-header";
+import ChooseDestinationOption from "../../components/2_choose-destination";
+import BoxMenu from "../../components/3_middle-box-menu";
+import TopDestinations from "../../components/top-destinations";
+import ServiceBlock from "../../components/services-block";
+import PopularDestinations from "../../components/popular-destinations";
+import FooterInstaBlock from "../../components/footer-insta";
+import Footer from "../../components/footer/footer";
+
+
+export default function Profile() {
+
+    return (
+        <div>
+            <div className="container">
+                    <MainHeader/>
+                    <ChooseDestinationOption/>
+                    <BoxMenu/>
+                    <TopDestinations/>
+                    <ServiceBlock/>
+                    <PopularDestinations/>
+                    <FooterInstaBlock/>
+                    <Footer/>
+                </div>
+            <div>
+                {/* <h3>{`Привет, ${currentUser.displayName}`}</h3>
+
+                <p>Ваш email: {currentUser.email}</p>
+                {SignInWithGoogle ? g_avatar : no_avatar}  */}
+            </div>
+            <br></br>
+            <div>
+                <button 
+                    className="btn btn-danger"
+                    onClick={() => signOut(auth)}>Выйти из аккаунта
+                </button>
+            </div>
+        </div>
+    )
+}
+
